5f45de192f86a7bc0560a5586305feac8fcbf8be
wyc <wyc>
2022-10-13 20:12:56 +0800

    做了几道题

---
+++ data-structure-algorithm/src/main/java/leetcode/top100_01/T021_mergeTwoLists.java
@@ -0,0 +1,34 @@
+package leetcode.top100_01;
+
+import leetcode.base.definition.ListNode;
+
+public class T021_mergeTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        while (list1 != null || list2 != null) {
+            if (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    tail.next = new ListNode(list1.val);
+                    tail = tail.next;
+                    list1 = list1.next;
+                } else {
+                    tail.next = new ListNode(list2.val);
+                    tail = tail.next;
+                    list2 = list2.next;
+                }
+            } else {
+                if (list1 != null) {
+                    tail.next = new ListNode(list1.val);
+                    tail = tail.next;
+                    list1 = list1.next;
+                }else{
+                    tail.next = new ListNode(list2.val);
+                    tail = tail.next;
+                    list2 = list2.next;
+                }
+            }
+        }
+        return head.next;
+    }
+}
